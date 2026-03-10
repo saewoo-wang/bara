d7c77f3c0716d2b5c57d97de69f4ac15e0d47ced
whya5448 <whya5448@gmail.com>
2026-03-11 00:51:18 +0900

    Initialize new Spring Boot project with Gradle configuration

---
+++ src/main/java/zip/saewoo/bara/BaraApplication.java
@@ -0,0 +1,13 @@
+package zip.saewoo.bara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zip/saewoo/bara/BaraApplicationTests.java
@@ -0,0 +1,13 @@
+package zip.saewoo.bara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
